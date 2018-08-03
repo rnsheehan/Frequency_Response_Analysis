5dba24e8d92cc609afc098256f480739ce0f2a68
rnsheehan <robertnsheehan@gmail.com>
2018-08-03 14:09:17 +0100

    Added external modules to the project. Started the main function.

--- Frequency_Response/Frequency_Response/Frequency_Response.py
+++ Frequency_Response/Frequency_Response/Frequency_Response.py
@@ -1,1 +1,30 @@
+# Import libraries
+# You should try an import the bare minimum of modules
+import sys # access system routines
+import os
+import glob
+import re
 
+import math
+import scipy
+import numpy
+import matplotlib.pyplot as plt
+
+# add path to our file
+sys.path.append('c:/Users/Robert/Programming/Python/Common/')
+sys.path.append('c:/Users/Robert/Programming/Python/Plotting/')
+
+import Common
+import Plotting
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+    pwd = os.getcwd() # get current working directory
+
+    print(pwd)
+
+    Common.test_import()
